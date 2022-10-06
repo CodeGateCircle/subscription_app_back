37a029d3a600957765720f96c0705ed36d077a8c
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-10-06 15:26:59 +0900

    fix table

--- db/migrate/20220906172851_create_users.rb
+++ db/migrate/20220906172851_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.references  :currency, null: false, foreign_key: true
-      t.references  :language, null: false, foreign_key: true
+      t.integer  :currency, limit:1, null: false,
+      t.integer  :language, limit:1, null: false,
 
       t.timestamps
     end
