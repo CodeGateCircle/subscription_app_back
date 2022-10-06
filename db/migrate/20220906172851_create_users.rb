38782a457e5ed59ceac3a4bc5192c43e9ec08b1b
yamato-1205 <ysakata1205@gmail.com>
2022-10-06 17:40:00 +0900

    create_user

--- db/migrate/20220906172851_create_users.rb
+++ db/migrate/20220906172851_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.integer  :currency, limit:1, null: false,
-      t.integer  :language, limit:1, null: false,
+      t.integer  :currency, limit:1, null: false
+      t.integer  :language, limit:1, null: false
 
       t.timestamps
     end
