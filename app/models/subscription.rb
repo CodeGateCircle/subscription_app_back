5779e40c67947d3e226e3706ebbb30e7754bebe4
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-10-08 00:27:49 +0900

    delete user_id

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,20 +1,20 @@
 class Subscription < ApplicationRecord
 	belongs_to :user, primary_key: :user_id
 
-	# validates :
+	# validates
 
 	enum payment_cycle: { oneMonth: 10, twoMonths: 20, threeMonths: 30, sixMonths: 60, year:120}
 	enum payment_method: { cash: 0, card: 1}
 
 	def format_res
         res = self.attributes.symbolize_keys
-        res.transform_keys!(user_id: :userId)
 		res.transform_keys!(first_payment_date: :firstPaymentDate)
 		res.transform_keys!(is_paused: :isPaused)
 		res.transform_keys!(image_url: :imageUrl)
 		res.transform_keys!(payment_cycle: :paymentCycle)
 		res.transform_keys!(payment_method: :paymentMethod)
 
+        res.delete(:user_id)
         res.delete(:created_at)
         res.delete(:updated_at)
         return res
