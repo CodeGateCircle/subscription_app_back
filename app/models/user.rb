1adde47511e8b9a350111cc34dd9de0cd66b2fb6
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-10-07 17:49:11 +0900

    add format

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
 
     enum currency: { JPY: 0, USD: 1}
     enum language: { Japanese: 0, English: 1}
+
+    def format_res
+        res = user_json = self.attributes.symbolize_keys
+        res.transform_keys!(user_id: :userId)
+        res.delete(:create_at)
+        res.delete(:update_at)
+        return res
+    end
 end
