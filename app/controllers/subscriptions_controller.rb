292501effa38342b693528ceb180219daf718619
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-10-07 17:39:39 +0900

    add iamge

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,8 @@
 class SubscriptionsController < ApplicationController
+    require './app/firestore/firestore'
 
     def create
         params = create_params
-
         subscription = Subscription.create({
             user_id: params[:userId],
             name: params[:subscription][:name],
@@ -10,11 +10,13 @@ class SubscriptionsController < ApplicationController
             first_payment_date: params[:subscription][:firstPaymentDate],
             remarks: params[:subscription][:remarks],
             is_paused: params[:subscription][:isPause],
-            image_url: params[:subscription][:imageUrl],
             payment_cycle: params[:subscription][:paymentCycle],
             payment_method: params[:subscription][:paymentMethod],
         })
-
+        url = Firestore.save_image_and_get_url(params[:subscription][:image], subscription.id.to_s)
+        subscription.update(
+            image_url: url
+        )
         render :json => {data:subscription}
     end
 
@@ -56,7 +58,7 @@ class SubscriptionsController < ApplicationController
 
     # strong parameter
     def create_params
-        params.permit(:userId, subscription: [:name, :price, :firstPaymentDate, :remarks, :isPause, :imageUrl, :paymentCycle, :paymentMethod])
+        params.permit(:userId, subscription: [:name, :price, :firstPaymentDate, :remarks, :isPause, :image, :paymentCycle, :paymentMethod])
     end
 
     def show_params
