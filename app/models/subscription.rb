85847757ece5b72fe69a871dc5c1b28ce72d1016
yamato-1205 <ysakata1205@gmail.com>
2022-10-07 16:10:00 +0900

    キャメルケースに変更

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
     belongs_to :user, primary_key: :user_id
 
-    enum payment_cycle: { one_month: 10, two_months: 20, three_Months: 30, six_months: 60, year:120}
+    enum payment_cycle: { oneMonth: 10, twoMonths: 20, threeMonths: 30, sixMonths: 60, year:120}
     enum payment_method: { cash: 0, card: 1}
 end
